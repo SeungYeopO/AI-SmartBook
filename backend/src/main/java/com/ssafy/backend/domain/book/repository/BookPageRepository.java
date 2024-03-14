4cfdd4c9fd82952092d27daacf87d45f1258caef
김민석 <kms278711@naver.com>
2024-03-14 11:35:25 +0900

    ✨ feat: #S10P22C101-168 단어 목록 조회(학습여부포함) api 구현
    Former-commit-id: 4caac1e3ffda4519147d402aa5d89c0acc3165f6

--- backend/src/main/java/com/ssafy/backend/domain/book/repository/BookPageRepository.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/repository/BookPageRepository.java
@@ -1,11 +1,3 @@
-package com.ssafy.backend.domain.book.repository;
-
-import com.ssafy.backend.domain.book.entity.BookPage;
-import com.ssafy.backend.domain.book.entity.BookPageSentence;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface BookPageRepository extends JpaRepository<BookPage, Long>, BookPageCustomRespository{
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:5a62ff8decdb2e4b4d6bbe3695c0127d1909b6d27b5b29be1fa4199f1f2927ba
+size 273
