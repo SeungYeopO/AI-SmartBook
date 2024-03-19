24bee5fe99f4c6a673df0eefa046b0080e9e7609
강대수 <sore503@naver.com>
2024-03-19 15:38:24 +0900

    ♻refactor: #S10P21C101-backend 평점 스코어 수정
    Former-commit-id: 06fd90de72f037d736eaf3283f9dd3bc17f94bdf

--- backend/src/main/java/com/ssafy/backend/domain/book/dto/response/BookReviewMyResponseDto.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/dto/response/BookReviewMyResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92f04d527e41628061b369940cd0b15236dd1b897d5d435fd38e09eea8f278da
-size 263
+oid sha256:6a9ea00a6498666c0b56f6312201692fbfa39d0e3913da41244b9e9a93a2cf93
+size 266
--- backend/src/main/java/com/ssafy/backend/domain/book/dto/response/BookReviewResponseDto.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/dto/response/BookReviewResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:541ad6e905df95fee09cea0495690bfe1087005f23485d196168b403a3b0b1dd
-size 171
+oid sha256:dfcf3db8149ed0588e1528731d17b8c1711a767790ab20ad7dafd97d7e25e68f
+size 174
