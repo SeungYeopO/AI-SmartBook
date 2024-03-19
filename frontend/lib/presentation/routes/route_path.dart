f2f620f76c7466583b02054eff8a78b16a45aaa5
김민주 <ducco705@snu.ac.kr>
2024-03-19 11:02:23 +0900

    :construction: feat: #S10P22C101-204 AI test 화면 구현중
    Former-commit-id: bffbd0753d44c3691b1e82d36f7ac1f5b1a0b86f

--- frontend/lib/presentation/routes/route_path.dart
+++ frontend/lib/presentation/routes/route_path.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be0f7238adf77310ae3ed05c806cc923c205aba8683fb38dae182f5ec5486938
-size 420
+oid sha256:6e2c478e24fc90b939f301b365cca7ac0ac0e7ce70332002ae1bad0c24b6e3cc
+size 463
