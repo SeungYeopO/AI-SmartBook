fbd4ba622f75141788d85b985721b7de7d70e0b2
김민석 <kms278711@naver.com>
2024-03-20 16:07:12 +0900

    ♻ refactor : #S10P22C101-317 코드 전체적 리팩토링
    Former-commit-id: 1e25f55f74dae31bf6e74d9649cb44be860614e6

--- backend/src/main/java/com/ssafy/backend/global/error/exception/TokenException.java
+++ backend/src/main/java/com/ssafy/backend/global/error/exception/TokenException.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e94eaebcb0073c2b18c9ca9c3efb25763759d28b7464d4825f8f049defa88e71
-size 439
+oid sha256:6b350c8cd7ff3d65cc148b1a8e39dae141914f757c2c2b2005b7cf152a2e815e
+size 406
