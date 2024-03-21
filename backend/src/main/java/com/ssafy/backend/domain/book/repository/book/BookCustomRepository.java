d4ec1cc9900f748d8b49129916f1a71acf895bee
강대수 <sore503@naver.com>
2024-03-21 16:57:08 +0900

    ✨feat: #S10P21C101-331 결제 내역 저장, 조회 구현
    Former-commit-id: 20a4eae550a29ddd191e8aa55cfdb46d55e85476

--- backend/src/main/java/com/ssafy/backend/domain/book/repository/book/BookCustomRepository.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/repository/book/BookCustomRepository.java
@@ -1,3 +1,9 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7a1d2882c3116ef22ef27c0c20e49a972ecb38092d1911b9c7517928c36eaec4
-size 249
+package com.ssafy.backend.domain.book.repository.book;
+
+import com.ssafy.backend.domain.book.dto.BookDto;
+
+import java.util.Optional;
+
+public interface BookCustomRepository {
+    Optional<BookDto> purchasedBookInfo(Long bookId, Long loginUSerId);
+}
--- backend/src/main/java/com/ssafy/backend/global/error/exception/DonggleException.java
+++ backend/src/main/java/com/ssafy/backend/global/error/exception/DonggleException.java
@@ -1,3 +1,19 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4104c3b8df34b0fe5d9f9a7dbd38da81d556e6f589c1b68892459c883b16d396
-size 410
+package com.ssafy.backend.global.error.exception;
+
+import lombok.Getter;
+
+@Getter
+public class DonggleException extends RuntimeException {
+
+	private final ExceptionType exceptionType;
+
+	public DonggleException(ExceptionType exceptionType) {
+		super(exceptionType.getErrorMessage());
+		this.exceptionType = exceptionType;
+	}
+
+	public int getStatus() {
+		return this.exceptionType.getHttpStatus().value();
+	}
+
+}
