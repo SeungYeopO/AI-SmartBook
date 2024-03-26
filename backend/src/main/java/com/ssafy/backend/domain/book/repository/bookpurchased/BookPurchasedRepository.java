40c9bd12ee5609f90350c028c8a8b72780f65c6d
강대수 <sore503@naver.com>
2024-03-26 16:20:48 +0900

    ♻ refactor: #S10P21C101-338 책 정보 조회 오류 수정
    Former-commit-id: 28a0934e05b2bface9a75ee5b45a9e76a3e05c12

--- backend/src/main/java/com/ssafy/backend/domain/book/repository/bookpurchased/BookPurchasedRepository.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/repository/bookpurchased/BookPurchasedRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f57eaf3173538403d85b99be8570a95def5a44c40f3400115daf51efff2b016
-size 480
+oid sha256:052a4f684e4a7dad4d3611ea2c9dc53fafea998dcbd3f7459f178e8fa6453315
+size 575
