7ae2008cbd6efe386f75760e421e6fd484290605
강대수 <sore503@naver.com>
2024-03-26 15:13:44 +0900

    ♻refactor: #S10P21C101-336 결제 내역 조회 수정
    Former-commit-id: 76c02b4e30cc127776585cd0b50e1590038dbd2d

--- backend/src/main/java/com/ssafy/backend/domain/book/dto/response/BookInfoResponseDto.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/dto/response/BookInfoResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bf47010f39e2fe823ad364c76dd4ba2437a77f9b8cbf0f071e73b7525a07885
-size 421
+oid sha256:58c561a306733c916df99644b8d6e6b2766a497367740c1b64e719674feb44d8
+size 382
