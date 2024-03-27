fdb5bad2cf7ff509fd82b452af8ce8e13bdb1ff6
강대수 <sore503@naver.com>
2024-03-27 13:52:07 +0900

    ♻refactor: #S10P21C101-338 동화책 조회 수정
    Former-commit-id: 5a99fe476cfd4a0b896e60998182651f5d588402

--- backend/src/main/java/com/ssafy/backend/domain/book/repository/book/BookCustomRepository.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/repository/book/BookCustomRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34242625620443c9fe64eef4fd8061cf1e823a10415a93cd5ee40c222fcd5b63
-size 300
+oid sha256:8f377c9d10b2b02474e7aa1e73eb358d6b00c01d3b7aa0651f87c0687001fcb0
+size 250
