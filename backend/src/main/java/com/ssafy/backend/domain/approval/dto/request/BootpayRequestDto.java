8aa3fad7e850296d9f3de64bc6d3b11a90d65ddb
김민석 <kms278711@naver.com>
2024-03-28 17:42:20 +0900

    ✨feat : #S10P22C101-374 이미지 아니면 업로드 안되게 수정, 이메일 검증 로직 추가
    Former-commit-id: 69ea798491fddb0297f5831757871b916f5ea91a

--- backend/src/main/java/com/ssafy/backend/domain/approval/dto/request/BootpayRequestDto.java
+++ backend/src/main/java/com/ssafy/backend/domain/approval/dto/request/BootpayRequestDto.java
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:534188e7e73a4312ec8b4abbc59cff6e8c5f2109bfc978d5dbb988f26f8ef06f
-size 138
+package com.ssafy.backend.domain.approval.dto.request;
+
+public record BootpayRequestDto(
+        String orderId,
+        int status
+) {
+}
