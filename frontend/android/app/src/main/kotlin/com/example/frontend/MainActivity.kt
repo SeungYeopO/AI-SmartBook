fc6deb65532f155b59f6b62eb0c3aef5f048a899
김민주 <ducco705@snu.ac.kr>
2024-04-04 00:01:49 +0900

    � docs: README.md 서비스 화면 수정
    Former-commit-id: 53b853673a4fa992de763356f7f11d81c18ebdcb

--- frontend/android/app/src/main/kotlin/com/example/frontend/MainActivity.kt
+++ frontend/android/app/src/main/kotlin/com/example/frontend/MainActivity.kt
@@ -1,5 +1,3 @@
-package com.example.frontend
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
+version https://git-lfs.github.com/spec/v1
+oid sha256:3e8d585ce81182b19cfd292ebeabf21f6864871057061e0f6f239d38bcd81df5
+size 121
